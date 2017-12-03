b86b46ea5c1dc87d13f677ba35fb41a2d9d86631
Alain Lompo <alain.lompo@gmail.com>
2017-12-03 21:09:34 +0100

    Adding a glass sphere on a plane

---
+++ starters/babylonians/playground/plane-and-glass-sphere.js
@@ -0,0 +1,44 @@
+var createScene = function() {
+	var scene = new BABYLON.Scene(engine);
+	    var camera = new BABYLON.ArcRotateCamera("Camera", -Math.PI / 4, Math.PI / 2.5, 200, BABYLON.Vector3.Zero(), scene);
+	camera.attachControl(canvas, true);
+    camera.minZ = 0.1;
+
+    // Environment Texture
+    var hdrTexture = BABYLON.CubeTexture.CreateFromPrefilteredData("textures/environment.dds", scene);
+
+    scene.imageProcessingConfiguration.exposure = 0.6;
+    scene.imageProcessingConfiguration.contrast = 1.6;
+
+    // Create meshes
+    var sphereGlass = BABYLON.Mesh.CreateSphere("sphereGlass", 48, 30.0, scene);
+    sphereGlass.translate(new BABYLON.Vector3(0, 1, 0), 15);
+
+    // Create materials
+    var glass = new BABYLON.PBRMaterial("glass", scene);
+    glass.reflectionTexture = hdrTexture;
+    glass.refractionTexture = hdrTexture;
+    glass.linkRefractionWithTransparency = true;
+    glass.indexOfRefraction = 0.52;
+    glass.alpha = 0;
+    glass.microSurface = 1;
+    glass.reflectivityColor = new BABYLON.Color3(0.2, 0.2, 0.2);
+    glass.albedoColor = new BABYLON.Color3(0.85, 0.85, 0.85);
+    sphereGlass.material = glass;
+
+    var woodPlank = BABYLON.MeshBuilder.CreateBox("plane", { width: 65, height: 1, depth: 65 }, scene);
+
+    var wood = new BABYLON.PBRMaterial("wood", scene);
+    wood.reflectionTexture = hdrTexture;
+    wood.environmentIntensity = 1;
+    wood.specularIntensity = 0.3;
+
+    wood.reflectivityTexture = new BABYLON.Texture("textures/reflectivity.png", scene);
+    wood.useMicroSurfaceFromReflectivityMapAlpha = true;
+
+    wood.albedoColor = BABYLON.Color3.White();
+    wood.albedoTexture = new BABYLON.Texture("textures/albedo.png", scene);
+    woodPlank.material = wood;
+
+	return scene;
+};
